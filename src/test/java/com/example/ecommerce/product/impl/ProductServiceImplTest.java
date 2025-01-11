46cb0658fc3d9237896d0fd540071c2b536c87f6
Juan Navarrete <navarrete0725@gmail.com>
2025-01-11 01:28:11 -0500

    adds test

--- src/test/java/com/example/ecommerce/product/impl/ProductServiceImplTest.java
+++ src/test/java/com/example/ecommerce/product/impl/ProductServiceImplTest.java
@@ -2,7 +2,9 @@ package com.example.ecommerce.product.impl;
 
 import com.example.ecommerce.exception.BadRequestException;
 import com.example.ecommerce.exception.GeneralException;
+import com.example.ecommerce.exception.ResourceNotFoundException;
 import com.example.ecommerce.product.dto.ProductRequest;
+import com.example.ecommerce.product.dto.ProductResponse;
 import com.example.ecommerce.product.entity.ProductEntity;
 import com.example.ecommerce.product.mapper.ProductMapper;
 import com.example.ecommerce.product.repository.ProductRepository;
@@ -12,6 +14,11 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -31,6 +38,7 @@ class ProductServiceImplTest {
 
     private ProductRequest productRequest;
     private ProductEntity productEntity;
+    private ProductResponse productResponse;
 
     @BeforeEach
     void setUp(){
@@ -49,6 +57,15 @@ class ProductServiceImplTest {
         productEntity.setDescription("Description");
         productEntity.setPrice(50.0);
         productEntity.setStock(100);
+
+        productResponse = new ProductResponse();
+        productResponse.setProductId("P001");
+        productResponse.setName("Product 1");
+        productResponse.setDescription("Description");
+        productResponse.setPrice(50.0);
+        productResponse.setStock(100);
+        productResponse.setCreatedAt(LocalDateTime.now());
+        productResponse.setUpdatedAt(LocalDateTime.now());
     }
 
     @Test
@@ -77,6 +94,52 @@ class ProductServiceImplTest {
 
         assertThrows(GeneralException.class, () -> {
             productService.createProduct(productRequest);
+        });
+    }
+
+    @Test
+    void testGetAllProductsSuccess() {
+        List<ProductEntity> productList = new ArrayList<>();
+        productList.add(productEntity);
+
+        when(productRepository.findAll()).thenReturn(productList);
+        when(productMapper.mapToProductResponse(productEntity)).thenReturn(productResponse);
+
+        List<ProductResponse> result = productService.getAllProducts();
+
+        assertNotNull(result);
+    }
+
+    @Test
+    void testGetAllGeneralError(){
+        when(productRepository.findAll()).thenThrow(new GeneralException("Database error"));
+        assertThrows(GeneralException.class, () -> {
+            productService.getAllProducts();
+        });
+    }
+
+    @Test
+    void testGetProductByIdSuccess() {
+        when(productRepository.findByProductId("P001")).thenReturn(Optional.of(productEntity));
+        when(productMapper.mapToProductResponse(productEntity)).thenReturn(productResponse);
+        ProductResponse result = productService.getProductById("P001");
+
+        assertNotNull(result);
+    }
+
+    @Test
+    void testGetProductByIdNotFound() {
+        when(productRepository.findByProductId("P002")).thenReturn(Optional.empty());
+        assertThrows(ResourceNotFoundException.class, () -> {
+            productService.getProductById("P002");
+        });
+    }
+
+    @Test
+    void testGetProductByIdGeneralError() {
+        when(productRepository.findByProductId(anyString())).thenThrow(new GeneralException("Database error"));
+        assertThrows(GeneralException.class, () -> {
+            productService.getProductById("P001");
         });
     }
 }
